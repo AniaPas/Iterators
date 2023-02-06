b25c9608ef9b268a83f91b384a2ea57bb8b5353d
Ania <an.pasieczna@gmail.com>
2023-02-06 01:42:54 +0100

    stiling compo1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Excercises from "./excercises/Excercises";
 import Excercise3 from "./excercises/Excercise3";
@@ -9,18 +8,9 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <Excercises />
         <Excercise3 />
         <Component1 />
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/excercises/Excercise4/Component1.tsx
+++ src/excercises/Excercise4/Component1.tsx
@@ -4,7 +4,15 @@ import style from "./Component1.module.scss";
 export const Component1: FC = () => {
   return (
     <div className={style.container}>
-      <h1>Monica Doe</h1>
+      <div className={style.imageContainer}></div>
+      <h1 className={style.name}>Monica Doe</h1>
+      <button className={style.plusBtn}>+</button>
+      <h2 className={style.positionName}>Position</h2>
+      <h2 className={style.position}>Front-end developer</h2>
+      <h2 className={style.companyName}>Company</h2>
+      <h2 className={style.company}>Adobe</h2>
+      <p className={style.number}>Employee # 2901346</p>
+      <button className={style.moreBtn}>...</button>
     </div>
   );
 };
