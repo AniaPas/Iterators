92c4cc5588a8dd50ad2857a6b039d955bba3c29c
Ania <an.pasieczna@gmail.com>
2023-02-07 11:36:24 +0100

    component 2 edit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Excercises from "./excercises/Excercises";
 import Excercise3 from "./excercises/Excercise3";
 import { Component1 } from "./excercises/Excercise4/Component1";
+import { Component2 } from "./excercises/Excercise4/Component2";
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       <header className='App-header'>
         <Excercises />
         <Excercise3 />
+        <div className='separator'></div>
         <Component1 />
+        <div className='separator'></div>
+        <Component2 />
       </header>
     </div>
   );
--- src/excercises/Excercise4/Component2.tsx
+++ src/excercises/Excercise4/Component2.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { FC } from "react";
+import style from "./Component2.module.scss";
 
-export const Component1: FC = () => {
-  return <div>Ex4</div>;
+export const Component2: FC = () => {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.btnWrapper}>
+        <div className={style.slideBtn}></div>
+      </div>
+      <div className={style.imgWrapper}></div>
+      <p className={style.ideasText}>IDEAS</p>
+    </div>
+  );
 };
